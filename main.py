7d8b78460d2c8fb80d913a5426f52c1d580dbfcc
Robin Visser <robinvisser92@live.nl>
2023-10-18 20:07:34 +0200

    adding images in body to same dir.

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def convert_msg_to_html(msg_file_path):
     msg = Message(msg_file_path)
     msg.save(html=True)
 
+    # Handle attachments (including images)
+    for attachment in msg.attachments:
+        attachment.save()  # This will save each attachment in the same directory as the .msg file
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert .msg to .html and .pdf")
+    parser = argparse.ArgumentParser(description="Convert .msg to .html and handle embedded images")
     parser.add_argument("input_msg", help="Path to the .msg file")
     args = parser.parse_args()
     input_msg = args.input_msg
 
     convert_msg_to_html(input_msg)
-
+    
 # Usage py .\main.py test.msg
